2623cb693a7001d743db928b4de3e94bc623b06a
Alex <salispiligrim@yandex.ru>
2026-02-03 14:43:03 +0500

    Добавил реакт роутинг

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Welcome from './pages/Welcome';
 
 const rootElement = document.getElementById('app');
 const root = createRoot(rootElement);
 
 root.render(
-    <StrictMode>
-        <Welcome />
-    </StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 );
